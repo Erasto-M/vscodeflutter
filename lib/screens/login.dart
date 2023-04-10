29b7db406195b085e75afe411c3a04f31909e500
erastus munyao kyalo <erastusmunya2022@gmail.com>
2023-04-10 12:08:45 +0300

    initial commit

---
+++ lib/screens/login.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _fnameController = TextEditingController();
+  final _lnameController = TextEditingController();
+  String? _fullName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calc era"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  controller: _fnameController,
+                  decoration:  InputDecoration(
+                    hintText: "First Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: _lnameController,
+                  decoration:  InputDecoration(
+                    hintText: "Last Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _fullName =
+                          '${_fnameController.text} ${_lnameController.text}';
+                    });
+                  },
+                  child: const Text("Submit details"),
+                ),
+                if (_fullName != null) ...[
+                  const SizedBox(height: 20),
+                  Text(_fullName!),
+                ],
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
